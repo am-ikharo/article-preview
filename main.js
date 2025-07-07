60cd9531306458d88c885d67310d3fd2872eb4f8
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-07-07 18:08:58 +0100

    added the js

---
+++ main.js
@@ -0,0 +1,7 @@
+const getButton = document.querySelector('.share-button')
+const getShare =document.querySelector('.share')
+
+
+getButton.addEventListener('click', () => {
+    getShare.style.display = 'block';
+})
